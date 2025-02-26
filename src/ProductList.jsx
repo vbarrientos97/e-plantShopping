ea36c333831ebe0160dfc3f0081325f0774a76e4
Verónica Barrientos <vbarrientos1997@gmail.com>
2025-02-25 21:04:43 -0500

    changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import {addItem} from './CartSlice';
+import CartSlice from './CartSlice';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
